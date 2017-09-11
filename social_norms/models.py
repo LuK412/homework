44d89d47c9b67c7fdeb99cda8c82bf82f013fd78
LuK412 <30651966+LuK412@users.noreply.github.com>
2017-09-11 17:35:41 +0200

    First steps

---
+++ social_norms/models.py
@@ -0,0 +1,151 @@
+from otree.api import (
+	models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+	Currency as c, currency_range
+)
+
+from django_countries.fields import CountryField
+
+author = 'Luisa'
+
+doc = """
+The experiment ends with a survey.
+"""
+
+
+class Constants(BaseConstants):
+	name_in_url = 'social_norms'
+	players_per_group = 3
+	num_rounds = len(player.my_group_id)
+
+	endowment = c(8)
+
+
+class Subsession(BaseSubsession):
+
+	def creating_session(self):
+		self.group_randomly
+
+
+	def before_session_starts(self):
+		for player in self.get_players():
+			player.treatment = self.session.config['treatment']
+			player.advice = self.session.config['advice']
+			
+
+	def creating_session(self):
+		group_matrix = self.get_group_matrix()
+		for group in group_matrix:
+			for player in group:
+				player.my_group_id = group_matrix.index(group) + 1
+
+
+class Group(BaseGroup):
+
+	decision_red = models.CharField(
+		choices=["A", "B", "C", "D", "E", "F"],
+		widget=widgets.RadioSelectHorizontal(),
+		verbose_name="Please make your decision.",
+		doc="red player makes his decision."
+		)
+
+	decision_blue = models.CharField(
+		choices=["A", "B", "C", "D", "E", "F"],
+		widget=widgets.RadioSelectHorizontal(),
+		verbose_name="Please indicate which allocation you would like red to choose.",
+		doc="blue players makes their decision."
+		)
+
+	decision_green = models.CharField(
+		choices=["A", "B", "C", "D", "E", "F"],
+		widget=widgets.RadioSelectHorizontal(),
+		verbose_name="Please indicate which allocation you would like red to choose.",
+		doc="green players makes their decision."
+		)
+
+	def calculate_payoffs(self):
+		red = self.get_player_by_role("red")
+		blue = self.get_player_by_role("blue")
+		green = self.get_player_by_role("green")
+
+		if self.in_round(1).decision_red == "A":
+			red.payoff = Constants.endowment + c(4)
+			blue.payoff = Constants.endowment
+			green.payoff = Constants.endowment - c(8)
+		if self.in_round(1).decision_red == "B":
+			red.payoff = Constants.endowment + c(2)
+			blue.payoff = Constants.endowment + c(2)
+			green.payoff = Constants.endowment - c(6)
+		if self.in_round(1).decision_red == "C":
+			red.payoff = Constants.endowment
+			blue.payoff = Constants.endowment + c(4)
+			green.payoff = Constants.endowment - c(4)
+		if self.in_round(1).decision_red == "D":
+			red.payoff = Constants.endowment - c(2)
+			blue.payoff = Constants.endowment + c(4)
+			green.payoff = Constants.endowment - c(2)
+		if self.in_round(1).decision_red == "E":
+			red.payoff = Constants.endowment - c(4)
+			blue.payoff = Constants.endowment + c(2)
+			green.payoff = Constants.endowment
+		if self.in_round(1).decision_red == "F":
+			red.payoff = Constants.endowment - c(6)
+			blue.payoff = Constants.endowment
+			green.payoff = Constants.endowment + c(2)
+
+
+class Player(BasePlayer):
+
+	my_group_id = models.IntegerField()
+
+	treatment = models.CharField()
+
+	advice = models.CharField()
+
+	payoff = models.CurrencyField(
+		doc="Payoff for the players"
+		)
+
+	def role(self):
+		if self.id_in_group == 1:
+			return "red"
+		if self.id_in_group == 2:
+			return "blue"
+		if self.id_in_group == 3:
+			return "green"
+
+
+	# ab hier der Fragebogen:
+	age = models.PositiveIntegerField(
+		max=100,
+		verbose_name="How old are you?",
+		doc="We ask participants for their age between 0 and 100 years"
+		)
+
+	gender = models.CharField(
+		choices=['female', 'male', 'other'],
+		widget=widgets.RadioSelect(),
+		verbose_name="Please indicate your gender.",
+		doc="gender indication"
+		)
+
+	studies = models.CharField(
+		blank=True,
+		verbose_name="Please indicate your field of studies.",
+		doc="field of studies indication."
+		)
+
+	studies2 = models.BooleanField(
+		widget=widgets.CheckboxInput(),
+		verbose_name="Non-student",
+		doc="Ticking the checkbox means that the participant is a non-student.")
+
+	risk = models.CharField(
+		choices=["Extrem risk averse", "risk averse", "somewhat risk averse", "neutral", "somewhat risk loving", "risk loving", "extreme risk loving"],
+		widget=widgets.RadioSelectHorizontal(),
+		verbose_name="Please indicate your risk preference.",
+		doc="7 point likert scale to measure risk preference."
+		)
+
+	country = CountryField(
+		blank=True,
+		verbose_name="Please indicate your country of birth.") # Kein doc und kein models. möglich davor.
---
+++ social_norms/views.py
@@ -0,0 +1,83 @@
+from otree.api import Currency as c, currency_range
+from . import models
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+class Instructions(Page):
+
+	def is_displayed(self):
+		return self.round_number == 1
+
+
+class Decision_red(Page):
+	form_model = models.Group
+	form_fields = ["decision_red"]
+
+	def is_displayed(self):
+		return self.player.role() == "red" and self.round_number == 1
+
+
+class Decision_blue(Page):
+	form_model = models.Group
+	form_fields = ["decision_blue"]
+
+	def is_displayed(self):
+		return self.player.role() == "blue" and self.round_number == 1
+
+class Decision_green(Page):
+	form_model = models.Group
+	form_fields = ["decision_green"]
+
+	def is_displayed(self):
+		return self.player.role() == "green" and self.round_number == 1
+
+class RevelationWaitPage(WaitPage):
+
+	wait_for_all_groups = True
+
+class Revelation(Page):
+
+	def is_displayed(self):
+		return self.session.config['treatment'] == "public"
+
+
+class WaitPage(WaitPage):
+
+	def after_all_players_arrive(self):
+		self.group.calculate_payoffs()
+
+
+class Results(Page):
+	
+	def is_displayed(self):
+		return self.round_number == Constants.num_rounds
+
+
+class Questionnaire(Page):
+
+	def is_displayed(self):
+		return self.round_number == Constants.num_rounds
+
+
+	form_model = models.Player
+	form_fields = ["age", "gender", "studies", "studies2", "risk", "country"]
+
+	def error_message(self, values):
+		if values["studies"] == "" and values["studies2"] != True:
+			return "Are you a non-student?"
+		if values["studies"] != "" and values["studies2"] == True:
+			return "You stated a field of studies, but indicated that you are a non-student."
+
+
+page_sequence = [
+	Instructions,
+	Decision_red,
+	Decision_blue,
+	Decision_green,
+	RevelationWaitPage,
+	Revelation,
+	WaitPage,
+	Results,
+	Questionnaire,
+]
